afb4a5f0fb2cd0c89f062e776b828e4872b372cd
Luis Mario Hernández-Soto <lhernandezs1212@egresado.ipn.mx>
2025-08-17 17:10:16 -0600

    Update ensamble_auto_genomas_ONT.sh

--- ONT/ensamble_auto_genomas_ONT.sh
+++ ONT/ensamble_auto_genomas_ONT.sh
@@ -26,3 +26,37 @@ cat genomas.txt | while read line;do #GENOMAS.TXT ES UNA LISTA CON LAS CARPETAS 
     canu -p $line -d assembly_canu_$line genomeSize=2.3m corThreads=20 -nanopore-raw $line.filtered.fastq # ENSAMBLA CON LAS CORRECCIONES
     cd ..
 done
+#!/bin/bash
+# Ensamble automatico de ONT
+
+# Lo primero es concatenar todos los fastq que se generaron por corrida
+# Eg.
+# cat barcode01/*.gz > ./barcode01.fastq.gz
+mkdir fastqc
+
+# Revisión de calidad
+echo "Vamos a echale un ojo a la calidad"
+fastqc -t 44 -o fastqc *.gz
+
+## Creando lista para revisar adaptadores y cortarlos
+echo "Creando lista para revisar adaptadores y cortarlos"
+ls *.fastq.gz > adaptadores.txt
+echo "Cortando adaptadores"
+cat adaptadores.txt | while read line; do
+    porechop_abi -t 40 --ab_initio -i $line -o  ${line%.gz}.porechoped.fastq.gz
+done
+
+# Revisión de calidad
+mkdir fastqc_porechop_abi
+fastqc -t 40 -o fastqc_porechop_abi *porechoped.fastq.gz
+
+# Creación de lista_genomas
+ls *porechoped.fastq.gz > porechoped_genomas.txt
+
+cat porechoped_genomas.txt | while read line;do #GENOMAS.TXT ES UNA LISTA CON LAS CARPETAS DONDE ESTAN LAS LECTURAS
+    echo "Puliendo " $line
+    seqkit seq -g -m 1000 $line -o $line.filtered.fastq.gz # QUITA READS MENORES A 1000 bp
+    echo "Ensamblando " $line
+    canu -p $line -d assembly_canu_$line genomeSize=2.3m corThreads=20 -nanopore-raw $line.filtered.fastq.gz # ENSAMBLA CON LAS CORRECCIONES
+done
+done
